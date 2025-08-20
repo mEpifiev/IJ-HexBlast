4cb6ee102aadfd2d580163d0d99581f3d3fd34cd
Marlen Epifiev <epifiev@gmail.com>
2025-08-20 19:38:14 +0300

    Create InputReder.cs

---
+++ Assets/Game/Scripts/Controls/InputReader.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace Game.Scripts.Controls
+{
+    public class InputReader : MonoBehaviour
+    {
+        private const int MouseButton = 0;
+        
+        private Camera _camera; 
+        
+        public event Action<Vector3> DragStarted;
+        public event Action<Vector3> Dragging;
+        public event Action<Vector3> DragEnded;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            Vector3 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+
+            if (Input.GetMouseButtonDown(MouseButton))
+                DragStarted?.Invoke(mousePosition);
+            
+            if(Input.GetMouseButton(MouseButton))
+                Dragging?.Invoke(mousePosition);
+            
+            if(Input.GetMouseButtonUp(MouseButton))
+                DragEnded?.Invoke(mousePosition);
+        } 
+    }
+}
